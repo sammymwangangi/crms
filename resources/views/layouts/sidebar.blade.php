944934b2a4b3cf7780a7c3cbc2ec52288863f4c1
Musioks <chandydavid88@gmail.com>
2018-03-15 21:33:52 +0300

    added some rstrictions on routes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,7 @@
-    <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+    @php $level=Auth::user()->user_level; @endphp
+    @switch($level)
+    @case($level==1)
+        <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
           <a class="nav-link" href="{{('/main')}}">
             <i class="fa fa-fw fa-dashboard"></i>
@@ -36,3 +39,75 @@
           </a>
         </li>
       </ul>
+        @break
+    @case($level==2)
+       <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="{{('/main')}}">
+            <i class="fa fa-fw fa-dashboard"></i>
+            <span class="nav-link-text">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Customers">
+          <a class="nav-link" href="{{url('main/customers')}}">
+            <i class="fa fa-fw fa-users"></i>
+            <span class="nav-link-text">Customers</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Transactions">
+          <a class="nav-link" href="{{url('main/transactions')}}">
+            <i class="fa fa-fw fa-money"></i>
+            <span class="nav-link-text">Transactions</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Profile">
+          <a class="nav-link" href="{{('main/profile')}}">
+            <i class="fa fa-fw fa-address-card-o"></i>
+            <span class="nav-link-text">Profile</span>
+          </a>
+        </li>
+      </ul>
+        @break
+
+    @default
+     <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="{{('/main')}}">
+            <i class="fa fa-fw fa-dashboard"></i>
+            <span class="nav-link-text">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Staff">
+          <a class="nav-link" href="{{url('main/staff')}}">
+            <i class="fa fa-fw fa-group"></i>
+            <span class="nav-link-text">Staff</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Customers">
+          <a class="nav-link" href="{{url('main/customers')}}">
+            <i class="fa fa-fw fa-users"></i>
+            <span class="nav-link-text">Customers</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Products">
+          <a class="nav-link" href="{{url('main/products')}}">
+            <i class="fa fa-fw fa-shopping-bag"></i>
+            <span class="nav-link-text">Products</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Transactions">
+          <a class="nav-link" href="{{url('main/transactions')}}">
+            <i class="fa fa-fw fa-money"></i>
+            <span class="nav-link-text">Transactions</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Profile">
+          <a class="nav-link" href="{{('main/profile')}}">
+            <i class="fa fa-fw fa-address-card-o"></i>
+            <span class="nav-link-text">Profile</span>
+          </a>
+        </li>
+      </ul>
+
+@endswitch
+   
